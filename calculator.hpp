78b955b4bf9fcf8e19946f4073687feadceb492c
Will Singleton <will5horace@ku.edu>
2025-04-16 23:28:26 -0500

    Finally we have finished commenting out the code from the calculator part with all the functions and modules. At the same time we have finished the header page which describes the function and design of the modules of the calculator part. We  define the variables used inside and how they might work.

--- calculator.hpp
+++ calculator.hpp
@@ -2,9 +2,11 @@
 #define __CALCULATOR_HPP
 
 #include <string>
+#include <vector>
 
-// this function should expect that `expression` has already been
-// validated and thus throw no exceptions
+bool validDouble(const std::string &str);
+std::string addDouble(const std::string &num1, const std::string &num2);
+std::vector<std::string> readNumFile(const std::string &filename);
 double parse_number(const std::string &expression);
 
 #endif // __CALCULATOR_HPP
